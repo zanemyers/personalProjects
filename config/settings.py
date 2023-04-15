b82c52a27c65c028971bad7fa0945db4091cfdb5
Zane Myers <zane@canopyteam.org>
2023-04-15 15:18:52 -0500

    fixed formatting junk for templates

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    # os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
